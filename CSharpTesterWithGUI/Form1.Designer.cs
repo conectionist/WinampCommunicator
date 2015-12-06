ae96035612b45d21db710df9a2e062c276500337
conectionist <dan_man_1_2003@yahoo.com>
2015-12-07 00:10:00 +0200

    Imported C functions in C# GUI tester project
    - added classes in C# project that call C functions (that use WinampCommunicator methods)
    - added buttons to dialog for every imported function (each function represents an action)

--- CSharpTesterWithGUI/Form1.Designer.cs
+++ CSharpTesterWithGUI/Form1.Designer.cs
@@ -28,34 +28,167 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.buttonNext = new System.Windows.Forms.Button();
+            this.buttonPrevious = new System.Windows.Forms.Button();
+            this.buttonPlay = new System.Windows.Forms.Button();
+            this.buttonPause = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.volumeBar = new System.Windows.Forms.TrackBar();
+            this.labelVolume = new System.Windows.Forms.Label();
+            this.buttonVolumeDown = new System.Windows.Forms.Button();
+            this.buttonVolumeUp = new System.Windows.Forms.Button();
+            this.labelCurrentTrackName = new System.Windows.Forms.Label();
+            this.buttonRefreshTitle = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.volumeBar)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // buttonNext
             // 
-            this.button1.Location = new System.Drawing.Point(110, 103);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(53, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Next";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.buttonNext.Location = new System.Drawing.Point(248, 12);
+            this.buttonNext.Name = "buttonNext";
+            this.buttonNext.Size = new System.Drawing.Size(57, 23);
+            this.buttonNext.TabIndex = 0;
+            this.buttonNext.Text = "Next";
+            this.buttonNext.UseVisualStyleBackColor = true;
+            this.buttonNext.Click += new System.EventHandler(this.buttonNext_Click);
+            // 
+            // buttonPrevious
+            // 
+            this.buttonPrevious.Location = new System.Drawing.Point(12, 12);
+            this.buttonPrevious.Name = "buttonPrevious";
+            this.buttonPrevious.Size = new System.Drawing.Size(57, 23);
+            this.buttonPrevious.TabIndex = 1;
+            this.buttonPrevious.Text = "Previous";
+            this.buttonPrevious.UseVisualStyleBackColor = true;
+            this.buttonPrevious.Click += new System.EventHandler(this.buttonPrevious_Click);
+            // 
+            // buttonPlay
+            // 
+            this.buttonPlay.Location = new System.Drawing.Point(71, 12);
+            this.buttonPlay.Name = "buttonPlay";
+            this.buttonPlay.Size = new System.Drawing.Size(57, 23);
+            this.buttonPlay.TabIndex = 2;
+            this.buttonPlay.Text = "Play";
+            this.buttonPlay.UseVisualStyleBackColor = true;
+            this.buttonPlay.Click += new System.EventHandler(this.buttonPlay_Click);
+            // 
+            // buttonPause
+            // 
+            this.buttonPause.Location = new System.Drawing.Point(130, 12);
+            this.buttonPause.Name = "buttonPause";
+            this.buttonPause.Size = new System.Drawing.Size(57, 23);
+            this.buttonPause.TabIndex = 3;
+            this.buttonPause.Text = "Pause";
+            this.buttonPause.UseVisualStyleBackColor = true;
+            this.buttonPause.Click += new System.EventHandler(this.buttonPause_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(189, 12);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(57, 23);
+            this.buttonStop.TabIndex = 4;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // volumeBar
+            // 
+            this.volumeBar.BackColor = System.Drawing.SystemColors.Control;
+            this.volumeBar.Location = new System.Drawing.Point(12, 41);
+            this.volumeBar.Maximum = 100;
+            this.volumeBar.Name = "volumeBar";
+            this.volumeBar.Size = new System.Drawing.Size(144, 45);
+            this.volumeBar.TabIndex = 5;
+            this.volumeBar.ValueChanged += new System.EventHandler(this.volumeBar_ValueChanged);
+            // 
+            // labelVolume
+            // 
+            this.labelVolume.AutoSize = true;
+            this.labelVolume.Location = new System.Drawing.Point(162, 52);
+            this.labelVolume.Name = "labelVolume";
+            this.labelVolume.Size = new System.Drawing.Size(51, 13);
+            this.labelVolume.TabIndex = 6;
+            this.labelVolume.Text = "Volume: -";
+            // 
+            // buttonVolumeDown
+            // 
+            this.buttonVolumeDown.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonVolumeDown.Location = new System.Drawing.Point(237, 43);
+            this.buttonVolumeDown.Name = "buttonVolumeDown";
+            this.buttonVolumeDown.Size = new System.Drawing.Size(31, 29);
+            this.buttonVolumeDown.TabIndex = 7;
+            this.buttonVolumeDown.Text = "-";
+            this.buttonVolumeDown.UseVisualStyleBackColor = true;
+            this.buttonVolumeDown.Click += new System.EventHandler(this.buttonVolumeDown_Click);
+            // 
+            // buttonVolumeUp
+            // 
+            this.buttonVolumeUp.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonVolumeUp.Location = new System.Drawing.Point(274, 43);
+            this.buttonVolumeUp.Name = "buttonVolumeUp";
+            this.buttonVolumeUp.Size = new System.Drawing.Size(31, 29);
+            this.buttonVolumeUp.TabIndex = 8;
+            this.buttonVolumeUp.Text = "+";
+            this.buttonVolumeUp.UseVisualStyleBackColor = true;
+            this.buttonVolumeUp.Click += new System.EventHandler(this.buttonVolumeUp_Click);
+            // 
+            // labelCurrentTrackName
+            // 
+            this.labelCurrentTrackName.AutoSize = true;
+            this.labelCurrentTrackName.Location = new System.Drawing.Point(12, 89);
+            this.labelCurrentTrackName.Name = "labelCurrentTrackName";
+            this.labelCurrentTrackName.Size = new System.Drawing.Size(93, 13);
+            this.labelCurrentTrackName.TabIndex = 9;
+            this.labelCurrentTrackName.Text = "Currently playing: -";
+            // 
+            // buttonRefreshTitle
+            // 
+            this.buttonRefreshTitle.Location = new System.Drawing.Point(237, 117);
+            this.buttonRefreshTitle.Name = "buttonRefreshTitle";
+            this.buttonRefreshTitle.Size = new System.Drawing.Size(73, 23);
+            this.buttonRefreshTitle.TabIndex = 10;
+            this.buttonRefreshTitle.Text = "Refresh title";
+            this.buttonRefreshTitle.UseVisualStyleBackColor = true;
+            this.buttonRefreshTitle.Click += new System.EventHandler(this.buttonRefreshTitle_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(322, 143);
+            this.Controls.Add(this.buttonRefreshTitle);
+            this.Controls.Add(this.labelCurrentTrackName);
+            this.Controls.Add(this.buttonVolumeUp);
+            this.Controls.Add(this.buttonVolumeDown);
+            this.Controls.Add(this.labelVolume);
+            this.Controls.Add(this.volumeBar);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonPause);
+            this.Controls.Add(this.buttonPlay);
+            this.Controls.Add(this.buttonPrevious);
+            this.Controls.Add(this.buttonNext);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.volumeBar)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonNext;
+        private System.Windows.Forms.Button buttonPrevious;
+        private System.Windows.Forms.Button buttonPlay;
+        private System.Windows.Forms.Button buttonPause;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.TrackBar volumeBar;
+        private System.Windows.Forms.Label labelVolume;
+        private System.Windows.Forms.Button buttonVolumeDown;
+        private System.Windows.Forms.Button buttonVolumeUp;
+        private System.Windows.Forms.Label labelCurrentTrackName;
+        private System.Windows.Forms.Button buttonRefreshTitle;
     }
 }
 
